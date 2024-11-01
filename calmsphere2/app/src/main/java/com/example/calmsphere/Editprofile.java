e1ee25242034405517a30ad8ed570863a3edb19e
Sushma-362 <sushma.edu2023@gmail.com>
2024-11-01 18:37:37 -0500

    EditProfile modified
    Added functionality to Editprofile class - S560218(Sushma)

--- calmsphere2/app/src/main/java/com/example/calmsphere/Editprofile.java
+++ calmsphere2/app/src/main/java/com/example/calmsphere/Editprofile.java
@@ -1,20 +1,34 @@
 package com.example.calmsphere;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class Editprofile extends AppCompatActivity {
+public class EditProfile extends BaseActivity {
+    private EditText nameProfile, emailProfile, phoneProfile, languageProfile;
+    private CheckBox dailyReminder;
+    private Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_editprofile);
 
+
+        nameProfile = findViewById(R.id.nameProfile);
+        emailProfile = findViewById(R.id.emailProfile);
+        phoneProfile = findViewById(R.id.phoneProfile);
+        languageProfile = findViewById(R.id.languageProfile);
+        dailyReminder = findViewById(R.id.dailyReminder);
+        btnLogout = findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(v -> {
+
+            Toast.makeText(EditProfile.this, "Logged out", Toast.LENGTH_SHORT).show();
+            finish();
+        });
     }
 }
